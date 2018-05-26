c458746f9b784986aeaf57b99ef764076dc18fbe
Theara Seng <seng.theara.kh@gmail.com>
2018-05-26 17:00:04 +0700

    define Query root type

---
+++ src/main/java/com/thearaseng/graphql/resolver/Query.java
@@ -0,0 +1,33 @@
+package com.thearaseng.graphql.resolver;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.thearaseng.graphql.model.Author;
+import com.thearaseng.graphql.model.Book;
+import com.thearaseng.graphql.repository.AuthorRepository;
+import com.thearaseng.graphql.repository.BookRepository;
+
+public class Query implements GraphQLQueryResolver {
+
+    private BookRepository bookRepository;
+    private AuthorRepository authorRepository;
+
+    public Query(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    public Iterable<Book> findAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Iterable<Author> findAllAuthors() {
+        return authorRepository.findAll();
+    }
+
+    public long countBooks() {
+        return bookRepository.count();
+    }
+    public long countAuthors() {
+        return authorRepository.count();
+    }
+}
